ff3a80c49958652ee6f9afbc8d56eba000843eb8
Ivan Dmitriev <vanya2003569@yandex.ru>
2025-05-22 23:52:44 +0300

    Добавить MeasurementCompletedEventArgs и добавить реализацию MeasuringDevice

---
+++ Chromatograph/Models/MeasurementCompletedEventArgs.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Chromatograph.Models;
+
+public class MeasurementCompletedEventArgs : EventArgs
+{
+
+    public MeasurementCompletedEventArgs()
+    {
+    }
+}
--- Chromatograph/Models/MeasuringDevice.cs
+++ Chromatograph/Models/MeasuringDevice.cs
@@ -7,10 +7,52 @@ namespace Chromatograph.Models;
 public class MeasuringDevice : Notifier
 {
     private DispatcherTimer _timer;
+    private int _currentPoint;
+    private int _amountOfData;
 
     public MeasuringDevice()
     {
         _timer = new DispatcherTimer();
         _timer.Interval = TimeSpan.FromMicroseconds(80);
+        _timer.Tick += OnStartMeasurement;
+    }
+
+    private void OnStartMeasurement(object? sender, EventArgs args)
+    {
+        if (IsRunning)
+            return;
+
+        if (_currentPoint >= _amountOfData)
+        {
+            _timer.Stop();
+            OnMeasurementCompleted();
+        }
+
+        ++CurrentPoint;
+    }
+
+    public void StartMeasurement(int amountOfdata)
+    {
+        _amountOfData = amountOfdata;
+        _currentPoint = 0;
+        _timer.Start();
+    }
+
+    public bool IsRunning
+    {
+        get { return _timer.IsEnabled; }
+    }
+
+    public int CurrentPoint
+    {
+        get { return _currentPoint; }
+        set { SetValue(ref _currentPoint, value, nameof(CurrentPoint)); }
+    }
+
+    public event EventHandler? MeasurementCompleted;
+    private void OnMeasurementCompleted()
+    {
+        if (MeasurementCompleted != null)
+            MeasurementCompleted.Invoke(this, new MeasurementCompletedEventArgs());
     }
 }
