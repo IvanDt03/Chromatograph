e9292536dbbf6c9069fb2c9c23c48d2acbfaa9f5
Ivan Dmitriev <vanya2003569@yandex.ru>
2025-05-22 22:44:29 +0300

    Добавить реализацию ViewModelBase

--- Chromatograph/ViewModels/ViewModelBase.cs
+++ Chromatograph/ViewModels/ViewModelBase.cs
@@ -1,12 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
-namespace Chromatograph.ViewModels
+namespace Chromatograph.ViewModels;
+
+public class ViewModelBase : INotifyPropertyChanged
 {
-    class ViewModelBase
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected virtual void OnPropertyChanged([CallerMemberName]string propertyName = "")
     {
+        if (PropertyChanged != null)
+            PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    protected virtual bool SetValue<T>(ref T field, T value, [CallerMemberName]string propertyName = "")
+    {
+        if (EqualityComparer<T>.Default.Equals(field, value))
+            return false;
+
+        field = value;
+        OnPropertyChanged(propertyName);
+        return true;
     }
 }
