25424ebd9dd5bc8d90bce10adf22418cfcb9e8b1
Ivan Dmitriev <vanya2003569@yandex.ru>
2025-05-23 15:30:49 +0300

    Добавить реализацию RealayCommand

--- Chromatograph/Commands/RelayCommand.cs
+++ Chromatograph/Commands/RelayCommand.cs
@@ -1,12 +1,41 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Windows.Input;
 
-namespace Chromatograph.Commands
+namespace Chromatograph.Commands;
+
+public class RelayCommand : ICommand
 {
-    class RelayCommand
+    private Action<object?>? _execute;
+    private Func<object?, bool>? _canExecute;
+
+    public RelayCommand(Action<object?>? execute, Func<object?, bool>? canExecute = null)
     {
+        if (execute == null)
+            throw new ArgumentNullException(nameof(execute));
+
+        _execute = execute;
+        _canExecute = canExecute;
+    }
+
+    public event EventHandler? CanExecuteChanged
+    {
+        add { CommandManager.RequerySuggested += value; }
+        remove { CommandManager.RequerySuggested -= value; }
+    }
+
+    public virtual void Execute(object? parameter)
+    {
+        if (_execute != null)
+            _execute.Invoke(parameter);
+    }
+
+    public virtual bool CanExecute(object? parameter)
+    {
+        return _canExecute?.Invoke(parameter) ?? true;
+    }
+
+    public void RaiseCanExecuteChanged()
+    {
+        CommandManager.InvalidateRequerySuggested();
     }
 }
--- Chromatograph/Service/IDataSerive.cs
+++ Chromatograph/Service/IDataSerive.cs
@@ -1,5 +1,9 @@
-﻿namespace Chromatograph.Service;
+﻿using Chromatograph.Models;
+using System.Collections.Generic;
+
+namespace Chromatograph.Service;
 
 public interface IDataSerive
 {
+    LoadResult<List<Polymer>> LoadPolymers();
 }
